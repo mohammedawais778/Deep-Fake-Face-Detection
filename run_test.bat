fa3c3d73c5ce6f8d8f597e3a1a0fbafeaa1a4a70
Mohammed Awais <154117907+mohammedawais778@users.noreply.github.com>
2025-06-12 18:47:14 +0530

    Delete run_test.bat

--- run_test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\Admin\Desktop\deepfake_detector"
-call venv_py310\Scripts\activate.bat
-python verify_env.py > env_check.log 2>&1
-python convert_to_torch.py --test_image "uploads\fake_183_253_4.jpg" > model_test.log 2>&1
-type env_check.log
-type model_test.log
